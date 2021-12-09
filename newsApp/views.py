e980c70b9f9841b761599dcb30467b18a3fadac7
yashraj3dot14 <yashraj.d.upadhyay@outlook.com>
2021-12-09 10:32:48 +0530

    Initial commit

---
+++ newsApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import requests
+
+API_KEY = '2e070a04d3da46f08b727ca9df1e6357'
+articles = ''
+
+# Create your views here.
+def home(request):
+    # url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}'
+    country = request.GET.get('country') #textbox value of search by country
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+    #print(data)
+    context = {'articles': articles}
+
+    return render(request,'newsApp/home.html', context)
